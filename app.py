685c84fe06f18d2c11c357f00eeebc5c3047ae55
Henry <hfsking@henrys-mbp.home>
2024-05-07 19:59:22 +0100

    rescale y axis

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import copy
 
 import numpy as np
 import pandas as pd
@@ -8,7 +9,7 @@ import datetime as dt
 import yfinance as yf
 
 from dash import Dash, dcc, html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -39,154 +40,161 @@ wti = yf.Ticker("CL=F")
 hist = wti.history(period="13y")
 oil_prices = hist.loc[inv.index.min():inv.index.max() + dt.timedelta(days=7)]
 
+# Graph object functions
 def build_market_trace(df: pd.DataFrame) -> list[BaseTraceType]:
-    
-    trace = go.Candlestick(x=df.index,
-                            open=df['Open'],
-                            high=df['High'],
-                            low=df['Low'],
-                            close=df['Close'],
-                          name='WTI Spot Price')
+
+    trace = go.Scatter(x=df.index,
+                       y=df['Close'],
+                       mode='lines',
+                       name='WTI spot price',
+                       fill='tozeroy')
     return [trace]
 
+def build_bar_trace(df: pd.DataFrame, col: str = 'Commercial Crude (Excluding SPR)') -> list[BaseTraceType]:
 
-def build_stock_bar_trace(df: pd.DataFrame) -> list[BaseTraceType]:
-    ## add the bars one at a time
     traces = []
 
-    inv = df['Weekly Commercial Draw/Build'].dropna().copy().to_frame('Change')
-    inv['Draw/Build'] = np.where(inv['Change'] >= 0, "Build", "Draw")
+    deltas = df[col].diff().dropna().copy().to_frame('Change')
+    deltas['Draw/Build'] = np.where(deltas['Change'] >= 0, "Build", "Draw")
     
     color_map = {"Build": 'red', "Draw": 'green'}
     
     for change in ['Draw', 'Build']:
-        sub_df = inv[inv['Draw/Build'] == change]
+        sub_df = deltas[deltas['Draw/Build'] == change]
         trace = go.Bar(x=sub_df.index, 
                        y=sub_df['Change'],
-                       width=5.184e8, # 6 days in milliseconds
+                       opacity=0.6,
+                       width=3.456e8, # 6 days in milliseconds
                        name=change,
-                       marker_color=color_map[change],
-                       opacity=0.6)
+                       marker_color=color_map[change])
         traces.append(trace)
+
     return traces
 
-def build_crude_line_trace(df: pd.DataFrame) -> list[BaseTraceType]:
+def build_line_trace(df: pd.DataFrame, col: str = 'Commercial Crude (Excluding SPR)') -> list[BaseTraceType]:
     trace = go.Scatter(x=df.index,
-                       y=df['Commercial Crude (Excluding SPR)'],
-                       name='Commercial Crude Inventory',
-                      fill='tozeroy')
+                       y=df[col],
+                       mode='lines',
+                       name=col,
+                       fill='tozeroy')
     return [trace]
 
-def build_spr_line_trace(df: pd.DataFrame) -> list[BaseTraceType]:
-    trace = go.Scatter(x=df.index,
-                       y=df['SPR'],
-                      mode='lines',
-                      name='SPR',
-                      fill='tozeroy')
-    return [trace]
+def build_figure(col: str = 'Commercial Crude (Excluding SPR)') -> go.Figure:
+    
+    fig = make_subplots(rows=3, 
+                        cols=1, 
+                        shared_xaxes=True,
+                        subplot_titles=("WTI Spot Price", f"{col} Change",
+                                        f"{col} Level"),
+                        vertical_spacing=0.05) 
 
+    fig.add_traces(build_market_trace(oil_prices), rows=1, cols=1)
+    fig.add_traces(build_bar_trace(inv, col), rows=2, cols=1)
+    fig.add_traces(build_line_trace(inv, col), rows=3, cols=1)
 
-fig = make_subplots(rows=4, 
-                    cols=1, 
-                    shared_xaxes=True,
-                    subplot_titles=("WTI Spot Price", "Commercial Stock Change",
-                                    "Commercial Stock Level", "SPR Stock Level"),
-                    vertical_spacing=0.05) 
-
-fig.add_traces(build_market_trace(oil_prices), rows=1, cols=1)
-fig.add_traces(build_stock_bar_trace(inv), rows=2, cols=1)
-# fig.add_traces(build_stock_box_trace(inv), rows=2, cols=1)
-fig.add_traces(build_crude_line_trace(inv), rows=3, cols=1)
-fig.add_traces(build_spr_line_trace(inv), rows=4, cols=1)
-
-fig.update_layout(
-    title_text="EIA US Crude Inventory Dashboard",
-    # autosize=True,
-    height=2000,
-    xaxis=dict(
-        # rangeselector=dict(
-        #     buttons=list([
-        #         dict(count=1,
-        #              label="1m",
-        #              step="month",
-        #              stepmode="backward"),
-        #         dict(count=6,
-        #              label="6m",
-        #              step="month",
-        #              stepmode="backward"),
-        #         dict(count=1,
-        #              label="YTD",
-        #              step="year",
-        #              stepmode="todate"),
-        #         dict(count=1,
-        #              label="1y",
-        #              step="year",
-        #              stepmode="backward"),
-        #         dict(step="all")
-        #     ])
-        # ),
-        rangeslider=dict(
-            visible=False
-        ),
-        type="date"
+    fig.update_layout(
+        autosize=True,
+        height=850,
+        showlegend=False,
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="YTD",
+                         step="year",
+                         stepmode="todate"),
+                    dict(count=1,
+                         label="1y",
+                         step="year",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=False
+            ),
+            type="date"
+        )
     )
-)
+    
+    return fig
+
 
 
 app = Dash()
+
 app.layout = html.Div([
-    dcc.Graph(figure=fig, id="fig")
+    html.H1(children='EIA Weekly Petroleum Status Report Dashboard', style={'textAlign':'center', 'fontFamily': 'sans-serif'}),
+    dcc.Dropdown(inv.columns, 'Commercial Crude (Excluding SPR)', id='Dropdown', style={'fontFamily':'sans-serif'}),
+    dcc.Graph(figure=build_figure(), id="Graph")
 ])
 
+def rescale_y_axis(figure, relay):
+    """
+    """
+
+    new_layout = copy.deepcopy(figure['layout'])
+    
+    if "xaxis.range[0]" in relay:
+
+        new_layout['xaxis3']['range'] = [relay['xaxis.range[0]'], relay['xaxis.range[1]']]
+    
+        x0 = dt.datetime.strptime(relay["xaxis.range[0]"][:10], '%Y-%m-%d')
+        x1 = dt.datetime.strptime(relay["xaxis.range[1]"][:10], '%Y-%m-%d')
+        x0 = wall_st.localize(x0)
+        x1 = wall_st.localize(x1)
+
+        # Price Data
+        i0 = np.searchsorted(figure['data'][0]['x'], x0)
+        i1 = np.searchsorted(figure['data'][0]['x'], x1)
+        miny = min(figure['data'][0]['y'][i0:i1])
+        maxy = max(figure['data'][0]['y'][i0:i1])
+        new_layout['yaxis']['range'] = [miny*0.98, maxy*1.01]
+
+        # Feature Change Data
+        i0 = np.searchsorted(figure['data'][1]['x'], x0)
+        i1 = np.searchsorted(figure['data'][1]['x'], x1)
+        miny = min(figure['data'][1]['y'][i0:i1])
+
+        i0 = np.searchsorted(figure['data'][2]['x'], x0)
+        i1 = np.searchsorted(figure['data'][2]['x'], x1)
+        maxy = max(figure['data'][2]['y'][i0:i1])
+        new_layout['yaxis2']['range'] = [miny*0.98, maxy*1.01]
+            
+        # Feature Level Data
+        i0 = np.searchsorted(figure['data'][3]['x'], x0)
+        i1 = np.searchsorted(figure['data'][3]['x'], x1)
+        miny = min(figure['data'][3]['y'][i0:i1])
+        maxy = max(figure['data'][3]['y'][i0:i1])
+        new_layout['yaxis3']['range'] = [miny*0.98, maxy*1.01]
+
+    figure['layout'] = new_layout
+    
+    return figure
+
 @app.callback(
-    Output("fig", "figure"),
-    Input("fig", "relayoutData"),
+    Output("Graph", "figure"),
+    Input('Dropdown', 'value'),
+    Input("Graph", "relayoutData"),
+    State('Graph', 'figure')
 )
-def scaleYaxis(rng):
-
-    if rng and "xaxis4.range[0]" in rng.keys():
-
-        try:
-            x0 = dt.datetime.strptime(rng["xaxis.range[0]"][:10], '%Y-%m-%d')
-            x1 = dt.datetime.strptime(rng["xaxis.range[1]"][:10], '%Y-%m-%d')
-            x0 = wall_st.localize(x0)
-            x1 = wall_st.localize(x1)
-
-            # Price Data
-            miny = oil_prices.loc[x0:x1, 'Low'].min()
-            maxy = oil_prices.loc[x0:x1, 'High'].max()
-            fig['layout']['yaxis']['range'] = [miny, maxy]
-
-            # Crude Change Data
-            miny = inv.loc[x0:x1, 'Weekly Commercial Draw/Build'].min()
-            maxy = inv.loc[x0:x1, 'Weekly Commercial Draw/Build'].max()
-            fig['layout']['yaxis2']['range'] = [miny, maxy]
-            
-            # Crude Inventory Data
-            miny = inv.loc[x0:x1, 'Commercial Crude (Excluding SPR)'].min()
-            maxy = inv.loc[x0:x1, 'Commercial Crude (Excluding SPR)'].max()
-            fig['layout']['yaxis3']['range'] = [miny, maxy]
-
-            # SPR Inventory Data
-            miny = inv.loc[x0:x1, 'SPR'].min()
-            maxy = inv.loc[x0:x1, 'SPR'].max()
-            fig['layout']['yaxis4']['range'] = [miny, maxy]
-            
-        except KeyError as e:
-            print(e)
-            pass
-
-            
-        finally:
-            
-            fig["layout"]["xaxis4"]["range"] = [rng["xaxis4.range[0]"], rng["xaxis4.range[1]"]]
-
+def plot_new_feature(feature, relay, fig):
+    if feature:
+        fig = build_figure(feature)
+    
+    if relay:
+        fig = rescale_y_axis(fig, relay)
+        
     return fig
 
 
 if __name__ == "__main__":
     app.run_server(debug=True)
-
-
-    
-
