c9b3da99c9cc0b5a33460c0f9f0930cfb81fd7e7
Henry <hfsking@henrys-mbp.home>
2024-05-08 08:34:43 +0100

    flake8 formatting

--- app.py
+++ app.py
@@ -29,15 +29,18 @@ def format_inventory_data(df: pd.DataFrame) -> pd.DataFrame:
     df.index = pd.to_datetime(df.index, format='%Y_%m_%d').tz_localize(wall_st)
     return df
 
+
 # Inventory Data
 stock_summary = pd.read_csv("./reports.csv", index_col=0)
 inv = format_inventory_data(stock_summary)
 inv['Commercial Crude (Excluding SPR)'] = inv['Crude Oil'] - inv['SPR']
 
+
 # Market Data
 wti = yf.Ticker("CL=F")
 hist = wti.history(period="13y")
 oil_prices = hist.loc[inv.index.min():inv.index.max() + dt.timedelta(days=7)]
+
 
 # Graph object functions
 def build_market_trace(df: pd.DataFrame) -> list[BaseTraceType]:
@@ -49,43 +52,53 @@ def build_market_trace(df: pd.DataFrame) -> list[BaseTraceType]:
                        fill='tozeroy')
     return [trace]
 
-def build_bar_trace(df: pd.DataFrame, col: str = 'Commercial Crude (Excluding SPR)') -> list[BaseTraceType]:
+
+def build_bar_trace(df: pd.DataFrame,
+                    col: str = 'Commercial Crude (Excluding SPR)'
+                    ) -> list[BaseTraceType]:
 
     traces = []
 
     deltas = df[col].diff().dropna().copy().to_frame('Change')
     deltas['Draw/Build'] = np.where(deltas['Change'] >= 0, "Build", "Draw")
-    
+
     color_map = {"Build": 'red', "Draw": 'green'}
-    
+
     for change in ['Draw', 'Build']:
         sub_df = deltas[deltas['Draw/Build'] == change]
-        trace = go.Bar(x=sub_df.index, 
+        trace = go.Bar(x=sub_df.index,
                        y=sub_df['Change'],
                        opacity=0.6,
-                       width=3.456e8, # 6 days in milliseconds
+                       width=3.456e8,  # 6 days in milliseconds
                        name=change,
                        marker_color=color_map[change])
+
         traces.append(trace)
 
     return traces
 
-def build_line_trace(df: pd.DataFrame, col: str = 'Commercial Crude (Excluding SPR)') -> list[BaseTraceType]:
+
+def build_line_trace(df: pd.DataFrame,
+                     col: str = 'Commercial Crude (Excluding SPR)'
+                     ) -> list[BaseTraceType]:
+
     trace = go.Scatter(x=df.index,
                        y=df[col],
                        mode='lines',
                        name=col,
                        fill='tozeroy')
+
     return [trace]
 
+
 def build_figure(col: str = 'Commercial Crude (Excluding SPR)') -> go.Figure:
-    
-    fig = make_subplots(rows=3, 
-                        cols=1, 
+
+    fig = make_subplots(rows=3,
+                        cols=1,
                         shared_xaxes=True,
                         subplot_titles=("WTI Spot Price", f"{col} Change",
                                         f"{col} Level"),
-                        vertical_spacing=0.05) 
+                        vertical_spacing=0.05)
 
     fig.add_traces(build_market_trace(oil_prices), rows=1, cols=1)
     fig.add_traces(build_bar_trace(inv, col), rows=2, cols=1)
@@ -126,27 +139,21 @@ def build_figure(col: str = 'Commercial Crude (Excluding SPR)') -> go.Figure:
             type="date"
         )
     )
-    
+
     return fig
 
-app = Dash()
-
-app.layout = html.Div([
-    html.H1(children='EIA Weekly Petroleum Status Report Dashboard', style={'textAlign':'center', 'fontFamily': 'sans-serif'}),
-    dcc.Dropdown(inv.columns, 'Commercial Crude (Excluding SPR)', id='Dropdown', style={'fontFamily':'sans-serif'}),
-    dcc.Graph(figure=build_figure(), id="Graph")
-])
 
 def rescale_y_axis(figure, relay):
     """
     """
 
     new_layout = copy.deepcopy(figure['layout'])
-    
+
     if "xaxis.range[0]" in relay:
 
-        new_layout['xaxis3']['range'] = [relay['xaxis.range[0]'], relay['xaxis.range[1]']]
-    
+        new_layout['xaxis3']['range'] = [relay['xaxis.range[0]'],
+                                         relay['xaxis.range[1]']]
+
         x0 = dt.datetime.strptime(relay["xaxis.range[0]"][:10], '%Y-%m-%d')
         x1 = dt.datetime.strptime(relay["xaxis.range[1]"][:10], '%Y-%m-%d')
         x0 = wall_st.localize(x0)
@@ -170,7 +177,7 @@ def rescale_y_axis(figure, relay):
         window_data = figure['data'][2]['y'][i0:i1]
         maxy = max(window_data) if window_data.size > 0 else 0
         new_layout['yaxis2']['range'] = [miny*0.98, maxy*1.01]
-            
+
         # Feature Level Data
         i0 = np.searchsorted(figure['data'][3]['x'], x0)
         i1 = np.searchsorted(figure['data'][3]['x'], x1)
@@ -179,24 +186,36 @@ def rescale_y_axis(figure, relay):
         new_layout['yaxis3']['range'] = [miny*0.98, maxy*1.01]
 
     figure['layout'] = new_layout
-    
+
     return figure
 
+
+app = Dash()
+
+app.layout = html.Div([
+    html.H1(children='EIA Weekly Petroleum Status Report Dashboard',
+            style={'textAlign': 'center', 'fontFamily': 'sans-serif'}),
+    dcc.Dropdown(inv.columns, 'Commercial Crude (Excluding SPR)',
+                 id='Dropdown', style={'fontFamily': 'sans-serif'}),
+    dcc.Graph(figure=build_figure(), id="Graph")
+])
+
+
 @app.callback(
-    Output("Graph", "figure"),
+    Output('Graph', 'figure'),
     Input('Dropdown', 'value'),
-    Input("Graph", "relayoutData"),
+    Input('Graph', 'relayoutData'),
     State('Graph', 'figure')
 )
 def plot_new_feature(feature, relay, fig):
     if feature:
         fig = build_figure(feature)
-    
+
     if relay:
         fig = rescale_y_axis(fig, relay)
-        
+
     return fig
 
 
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=8050, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8050, debug=True)
--- async_scrape_eia.py
+++ async_scrape_eia.py
@@ -14,124 +14,140 @@ from typing import Optional
 from time import perf_counter
 
 
-EIA_BASE_URL = "https://www.eia.gov"
-EIA_ARCHIVE_URI = "/petroleum/supply/weekly/archive/"
-EIA_REPORT_TABLE_URI = "/csv/table1.csv"
+EIA_BASE_URL = 'https://www.eia.gov'
+EIA_ARCHIVE_URI = '/petroleum/supply/weekly/archive/'
+EIA_REPORT_TABLE_URI = '/csv/table1.csv'
 
 PETROLEUM_STOCK_INDEX_TOP_LEVEL = [
-    "Crude Oil", 
-    "Total Motor Gasoline", 
-    "Fuel Ethanol", 
-    "Kerosene-Type Jet Fuel", 
-    "Distillate Fuel Oil",
-    "Residual Fuel Oil", 
-    "Propane/Propylene",
-    "Other Oils",
-    "Unfinished Oils", 
-    "Total Stocks (Including SPR)", 
-    "Total Stocks (Excluding SPR)"
+    'Crude Oil',
+    'Total Motor Gasoline',
+    'Fuel Ethanol',
+    'Kerosene-Type Jet Fuel',
+    'Distillate Fuel Oil',
+    'Residual Fuel Oil',
+    'Propane/Propylene',
+    'Other Oils',
+    'Unfinished Oils',
+    'Total Stocks (Including SPR)',
+    'Total Stocks (Excluding SPR)'
 ]
+
 
 def get_report_urls(earliest: Optional[dt.datetime] = None) -> list[str]:
     '''Generate EIA archive URLs for WPSRs
     '''
     resp = requests.get(urljoin(EIA_BASE_URL, EIA_ARCHIVE_URI))
-    
+
     if resp.status_code != 200:
-        
-        raise Exception(f"Failed to retrieve report urls with status code {resp.status_code}")
-        
+
+        raise Exception(
+            'Failed to retrieve report urls with status code {}'
+            .format(resp.status_code)
+            )
+
     contents = resp.content.decode(encoding='cp1252')
 
-    report_pattern = re.compile("(/petroleum/supply/weekly/archive/[0-9]{4}/[0-9]{4}_[0-9]{1,2}_[0-9]{1,2}(?:_data)?)")
-    
+    report_pattern = re.compile(
+        '(/petroleum/supply/weekly/archive/[0-9]{4}/'
+        '[0-9]{4}_[0-9]{1,2}_[0-9]{1,2}(?:_data)?)'
+        )
+
     report_uris = report_pattern.findall(contents)
-    
+
     if isinstance(earliest, dt.datetime):
-        
+
         recent_reports = []
-        
+
         for uri in report_uris:
-            
-            report_date = dt.datetime.strptime(uri.split('/')[-1].replace('_data', ''), '%Y_%m_%d')
-            
+
+            report_date = dt.datetime.strptime(
+                uri.split('/')[-1].replace('_data', ''),
+                '%Y_%m_%d'
+                )
+
             if report_date >= earliest:
-                
+
                 recent_reports.append(uri)
-                
+
         report_uris = recent_reports
-    
-    urls = [urljoin(EIA_BASE_URL, uri) + EIA_REPORT_TABLE_URI for uri in report_uris]
-    
+
+    urls = [urljoin(EIA_BASE_URL, uri) + EIA_REPORT_TABLE_URI
+            for uri in report_uris]
+
     return urls
-    
+
+
 async def download_table(session, url, semaphore) -> tuple[str, pd.DataFrame]:
     '''Send a request, throttling with Semaphore
     '''
     async with semaphore, session.get(url) as response:
-        
+
         with io.StringIO(await response.text(encoding='cp1252')) as text_io:
-            
+
             return url, pd.read_csv(text_io, on_bad_lines='skip')
+
 
 async def download_many_tables(urls, concur_req):
     '''Asynchronously download all csv files from urls
     '''
     df = pd.DataFrame(columns=PETROLEUM_STOCK_INDEX_TOP_LEVEL)
-    
+
     semaphore = asyncio.Semaphore(concur_req)
 
     async with aiohttp.ClientSession() as session:
-        
+
         to_do = [download_table(session, url, semaphore)
                  for url in urls]
-        
+
         to_do_itr = asyncio.as_completed(to_do)
 
         for coro in to_do_itr:
-            
+
             this_url, this_df = await coro
-            
-            try: # normalise table format
+
+            try:  # normalise table format
                 this_df = format_report_table(this_df)
                 this_df['Endpoint'] = [this_url]
-                
-            except KeyError: # invalid table format
-                print(f"Failed to extract table from {this_url}")
+
+            except KeyError:  # invalid table format
+                print(f'Failed to extract table from {this_url}')
                 pass
-                
-            else: # merge dataframes
-                
-                df = this_df.copy() if df.empty else pd.concat([df, this_df], axis=0)
-               
+
+            else:  # merge dataframes
+                df = (this_df.copy() if df.empty
+                      else pd.concat([df, this_df], axis=0))
+
         df.index = pd.to_datetime(df.index, format='%m/%d/%y')
-    
+
         return df
 
+
 def format_report_table(df: pd.DataFrame) -> dict:
-    """Extracts the top level stock inventories from a Petroleum Stocks DataFrame
+    """Extracts the top level stock inventories from
+     a Petroleum Stocks DataFrame
     """
     # Remove second-level stock rows
     summary_df = df[df['STUB_1'].isin(PETROLEUM_STOCK_INDEX_TOP_LEVEL)]
-    
+
     # Get latest stock levels (first column after STUB_1)
     stock_s = summary_df.set_index('STUB_1').iloc[:, 0]
-    
+
     stock_s.index.name = None
-    
+
     # Convert str values to float
     stock_s = stock_s.str.replace(',', '').astype(float)
-    
+
     # Reformat as df row
     stock_df = stock_s.to_frame().T
-    
+
     # Add SPR column
     stock_df['SPR'] = (stock_df['Total Stocks (Including SPR)']
                        - stock_df['Total Stocks (Excluding SPR)'])
-    
+
     return stock_df[PETROLEUM_STOCK_INDEX_TOP_LEVEL + ['SPR']]
-    
-def save_inventory_table_to_file(df, cache_file:Optional[str]=None):
+
+
+def save_inventory_table_to_file(df, cache_file: Optional[str] = None):
     '''Save inventory dataframe to file
     '''
     if cache_file is None:
@@ -139,43 +155,47 @@ def save_inventory_table_to_file(df, cache_file:Optional[str]=None):
 
     df.to_csv(cache_file)
 
-def download_eia_reports(urls, concur_req:int = 10):
+
+def download_eia_reports(urls, concur_req: int = 10):
     '''Function to run coroutines that download WPRS reports
     '''
     coro = download_many_tables(urls, concur_req)
-    
+
     df = asyncio.run(coro)
-    
+
     return df
 
-def load_cached_reports(cache_file) -> pd.DataFrame:
+
+def load_cached_reports(cache_file: str) -> pd.DataFrame:
     # check valid path here
     if os.path.isfile(cache_file):
-        print("Cache found")
+        print('Cache found')
         df = pd.read_csv(cache_file, index_col=0)
         df.index = pd.to_datetime(df.index)
     else:
         df = pd.DataFrame()
-    
+
     return df
 
 
-def add_latest_report_if_new(old_df: pd.DataFrame()) -> pd.DataFrame: 
+def add_latest_report_if_new(old_df: pd.DataFrame) -> pd.DataFrame:
     # Check for new release (non-archive)
     latest_url = 'https://ir.eia.gov/wpsr/table1.csv'
     latest_df = download_eia_reports([latest_url], concur_req=1)
-    
+
     if latest_df.index[-1] != old_df.index[-1]:
         # New release
-        df = latest_df.copy() if old_df.empty else pd.concat([old_df, latest_df], axis=0)
+        df = (latest_df.copy() if old_df.empty
+              else pd.concat([old_df, latest_df], axis=0))
     else:
-        print("Latest release already cached")
+        print('Latest release already cached')
         df = old_df
 
     return df
 
 
-def download_wpsr_since_date(date: dt.datetime, cache_file:Optional[str]=None) -> pd.DataFrame:
+def download_wpsr_since_date(date: dt.datetime,
+                             cache_file: Optional[str] = None) -> pd.DataFrame:
     # Get valid report endpoints
     report_urls = get_report_urls(earliest=date)
 
@@ -202,19 +222,19 @@ def download_wpsr_since_date(date: dt.datetime, cache_file:Optional[str]=None) -
     save_inventory_table_to_file(df)
     return df
 
-    
+
 if __name__ == '__main__':
 
     start_t = perf_counter()
 
     cache_file = os.path.join(os.getcwd(), 'reports.csv')
-    
-    cutoff =  dt.datetime.now() - dt.timedelta(weeks=104)
+
+    cutoff = dt.datetime.now() - dt.timedelta(weeks=104)
 
     df = download_wpsr_since_date(cutoff, cache_file=cache_file)
-    
-    print(f"Finished scraping EIA WPSRs")
-    print(f"{len(df)} reports found")
-    seconds = perf_counter() - start_t
-    print(f"That took {seconds//60:.0f}mins, {seconds%60:.2f}seconds", flush=True)
 
+    print('Finished scraping EIA WPSRs')
+    print(f'{len(df)} reports found')
+    seconds = perf_counter() - start_t
+    print(f'That took {seconds//60:.0f}mins, {seconds%60:.2f}seconds',
+          flush=True)
